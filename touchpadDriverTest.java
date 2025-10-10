d776d9e7d0fe69a35cf6bd304b2514d1caaa4323
Noah <kidclark27@gmail.com>
2025-10-10 17:02:07 -0400

    Update touchpadDriverTest.java

--- touchpadDriverTest.java
+++ touchpadDriverTest.java
@@ -1,3 +1,11 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
 public class TouchpadDriverTest1 {
     public static void main() {
     if(gamepad.1_touchpad_finger_1){
